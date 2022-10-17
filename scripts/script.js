c32063d0ffa66d26bf7d2afa45bb7d304301321b
Temmatema <112764243+Temmatema@users.noreply.github.com>
2022-10-17 21:44:06 +0300

    стили для главной страницы

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-var modal = document.getElementById('modal-window');
-var btn = document.getElementById('open-modal-window');
-var span = document.getElementsByClassName("close-modal-window")[0];
+let modal = document.getElementById('modal-window');
+let btn = document.getElementById('open-modal-window');
+let span = document.getElementsByClassName("close-modal-window")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
@@ -14,4 +14,17 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+
+let heart = document.querySelector('.button-like');
+let likesNumber = document.querySelector('.likes-number');
+
+heart.onclick = function() {
+    if (heart.classList.contains('added')) {
+        likesNumber.textContent--;
+    } else {
+        likesNumber.textContent++;
+    }
+    heart.classList.toggle('added');
 }
